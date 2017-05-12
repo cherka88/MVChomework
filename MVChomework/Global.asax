b7d90255d58a81db0cc784a8cf332b9addf21bec
lin profiteer <terence@linde-MacBook-Pro.local>
2017-05-12 23:17:12 +0800

    v1

---
+++ MVChomework/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVChomework.MvcApplication" Language="C#" %>
---
+++ MVChomework/Models/ValidationAttributes/驗證電話號碼Attribute.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+
+namespace MVChomework.Models.ValidationAttributes
+{
+    public class 驗證電話號碼Attribute : DataTypeAttribute
+    {
+        private static Regex _regx = new Regex(@"\d{4}-\d{6}", RegexOptions.IgnoreCase);
+        public 驗證電話號碼Attribute() : base(DataType.Text)
+        {
+
+        }
+
+        public override bool IsValid(object value)
+        {
+            if(value == null)
+            {
+                return true;
+            }
+            var str = value.ToString();
+            return _regx.Match(str).Length > 0;
+        }
+    }
+}
